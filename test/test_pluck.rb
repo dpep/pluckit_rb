0f5b6e6790313e425b17a18215097649b8f1812e
dpepper <pepper.daniel@gmail.com>
2017-10-03 22:36:03 -0700

    add more tests on the core

---
+++ test/test_pluck.rb
@@ -0,0 +1,72 @@
+require 'minitest/autorun'
+require 'pluckit'
+
+
+class PluckItTest < Minitest::Test
+
+
+  def test_array
+    data = [ 1, 2, 3 ]
+
+    assert_equal(
+      1,
+      PluckIt.pluck(data, 0)
+    )
+
+    assert_equal(
+      2,
+      PluckIt.pluck(data, 1)
+    )
+
+    assert_equal(
+      [ 1, 2 ],
+      PluckIt.pluck(data, 0..1)
+    )
+
+    assert_equal(
+      [ 'a', 'ba', 'ca' ],
+      PluckIt.pluck(
+        [ 'a', 'ba', 'bb', 'bc', 'ca' ],
+        /a/
+      )
+    )
+  end
+
+
+  def test_hash
+    data = {
+      a: 1,
+      b: 2,
+      c: 3,
+    }
+
+    assert_equal(
+      2,
+      PluckIt.pluck(data, :b)
+    )
+
+    assert_nil(
+      PluckIt.pluck(data, :z)
+    )
+  end
+
+
+  def test_obj
+    assert_equal(
+      123,
+      PluckIt.pluck(ABC.new, :foo)
+    )
+
+    assert_equal(
+      ABC,
+      PluckIt.pluck(ABC.new, :class)
+    )
+  end
+
+
+end
+
+
+class ABC
+  def foo() 123 end
+end
