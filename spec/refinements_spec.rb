606bf56133b0c17ad26467bba56d12868b3a51ab
Daniel Pepper <pepper.daniel@gmail.com>
2023-12-27 22:52:12 -0800

    .

--- spec/refinements_spec.rb
+++ spec/refinements_spec.rb
@@ -1,9 +1,13 @@
 describe PluckIt, :before_patch do
-  using PluckIt
+  it { expect([].respond_to?(:pluck)).to be false }
 
-  it { expect([].respond_to?(:pluck)).to be true }
-  it { expect({}.respond_to?(:pluck)).to be true }
-  it { expect(Set.new.respond_to?(:pluck)).to be true }
+  context "when refined" do
+    using PluckIt
 
-  it { expect([ 1, 2, 3 ].pluck(:to_s)).to eq([ "1", "2", "3" ]) }
+    it { expect([].respond_to?(:pluck)).to be true }
+    it { expect({}.respond_to?(:pluck)).to be true }
+    it { expect(Set.new.respond_to?(:pluck)).to be true }
+
+    it { expect([ 1, 2, 3 ].pluck(:to_s)).to eq([ "1", "2", "3" ]) }
+  end
 end
